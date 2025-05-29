417fc8b0d5cd91a8c899b84d4a4d32eed0844ff5
cluic <88969402+cluic@users.noreply.github.com>
2025-05-29 11:37:47 +0800

    更换群二维码

--- upload.bat
+++ upload.bat
@@ -1,5 +1,9 @@
+@echo off
+set commit_msg=%1
+if "%commit_msg%"=="" set commit_msg=update
+
 hugo --gc --minify
 git add .
-git commit -m "update"
+git commit -m "%commit_msg%"
 git push origin main
 scp -r -i C:\Users\Xingh\.ssh\hk_id_rsa public/* root@38.22.90.139:/opt/1panel/apps/openresty/openresty/www/sites/plus.wxauto.org/index
